d031cda84c84c648547fe309ecb4e0bdb0a7861f
cdcportal <cdc_portal@iitism.ac.in>
2023-03-12 18:51:08 +0530

    Added dotenv support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+if(process.env.NODE_ENV !== 'production'){
+  dotenv.config({
+    path: path.join(__dirname, '../.env'),
+  });
+}
+
 const app = express();
  
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello World," + process.env.sec1);
 });
  
-app.listen(3000, () => console.log("Listening on 3000!"));
+app.listen(3000, () => console.log("Listening at 3000!"));
+
+export default app;
